a3c862c44634bb272922767dae21018edf7726a9
Meindert <meindertduin@hotmail.com>
2020-09-17 15:26:30 +0200

    added basic watchgroup for display

---
+++ web-client/components/Account/watch-group.vue
@@ -0,0 +1,54 @@
+﻿<template>
+  <v-card
+    color="#385F73"
+    dark
+  >
+    <v-card-title class="headline">{{title}}</v-card-title>
+
+    <v-card-subtitle>Een beschrijving</v-card-subtitle>
+    <v-card-text>
+      <subtitle-1>Shared with</subtitle-1>
+      <v-list-group
+        active-class="deep-purple accent-4 white--text"
+        column
+      >
+        <v-list-item v-for="x in sharedWith">
+          <v-list-item-avatar color="blue">
+            <img v-if="x.picture.length != 0" :src="x.picture" alt="">
+            <span v-else class="white--text headline">{{x.userName[0].toUpperCase()}}</span>
+          </v-list-item-avatar>
+          <v-list-item-title>
+            {{x.userName}}
+          </v-list-item-title>
+        </v-list-item>
+
+      </v-list-group>
+    </v-card-text>
+    <v-card-actions>
+
+      <v-btn text>Edit</v-btn>
+      <v-btn text>Watch Now!</v-btn>
+    </v-card-actions>
+  </v-card>
+
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "nuxt-property-decorator";
+
+  interface Person {
+    userName: string,
+    link: string | null,
+    picture: string,
+  }
+
+    @Component({})
+    export default class WatchGroup extends Vue{
+      @Prop({type: String, required: true}) readonly title!: string
+      @Prop({type: Array, required: true}) readonly sharedWith!: Array<Person>
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ web-client/pages/account.vue
@@ -0,0 +1,48 @@
+﻿<template>
+  <v-container>
+    <v-row  class="justify-center">
+      <v-col :cols="5">
+        <WatchGroup :title="title" :shared-with="sharedWith"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "nuxt-property-decorator";
+  import WatchGroup from "~/components/Account/watch-group.vue";
+
+  interface Person {
+    userName: string,
+    link: string | null,
+    picture: string,
+  }
+
+
+  @Component({
+    name: "account",
+    components: {
+      WatchGroup
+    },
+  })
+  export default class Account extends Vue{
+    private title:string = "title";
+
+
+    private person: Person = {
+      userName: "john",
+      link: "https://localhost:3000",
+      picture: "https://m.media-amazon.com/images/M/MV5BMGE4YzY4NGEtOWYyYS00ZDk2LWExMmMtZDIyODhiMmNlMGE0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+    }
+
+    private sharedWith: Array<Person> = [];
+
+    created(){
+      this.sharedWith = [this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person, this.person]
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
