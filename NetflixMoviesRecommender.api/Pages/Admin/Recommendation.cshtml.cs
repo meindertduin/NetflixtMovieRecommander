4267859fa561260f439e700e06bc91d20dce2114
Meindert <meindertduin@hotmail.com>
2020-09-29 10:43:11 +0200

    admin page displays netflix recommendations and added delete functionality

--- NetflixMoviesRecommender.api/Pages/Admin/Recommendation.cshtml.cs
+++ NetflixMoviesRecommender.api/Pages/Admin/Recommendation.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using NetflixMovieRecommander.Data;
 using NetflixMovieRecommander.Models;
 using NetflixMoviesRecommender.api.Services;
 using Newtonsoft.Json;
@@ -14,16 +17,20 @@ namespace NetflixMoviesRecommender.api.Pages.Admin
     {
         private readonly IConfiguration _configuration;
         private readonly IRecommendedDatabaseParser _recommendedDatabaseParser;
+        private readonly AppDbContext _ctx;
         [BindProperty] public string Title { get; set; }
-
-        public Recommendation(IConfiguration configuration, IRecommendedDatabaseParser recommendedDatabaseParser)
+        [BindProperty] public List<NetflixRecommended> Recommendations { get; set; }
+        
+        public Recommendation(IConfiguration configuration, IRecommendedDatabaseParser recommendedDatabaseParser, AppDbContext ctx)
         {
             _configuration = configuration;
             _recommendedDatabaseParser = recommendedDatabaseParser;
+            _ctx = ctx;
         }
         
         public IActionResult OnGet()
         {
+            Recommendations = _ctx.NetflixRecommendations.OrderBy(x => x.Title).ToList();
             return Page();
         }
 
@@ -52,13 +59,31 @@ namespace NetflixMoviesRecommender.api.Pages.Admin
             try
             {
                 await _recommendedDatabaseParser.StoreRecommendedToDatabase(recommended);
+                return RedirectToPage();
             }
             catch (Exception e)
             {
                 // ignored
             }
-
+    
             return Page();
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            var recommendation = _ctx.NetflixRecommendations.FirstOrDefault(x => x.Id == id);
+            if (recommendation != null)
+            {
+                _ctx.NetflixRecommendations.Remove(recommendation);
+                _ctx.SaveChanges();
+            }
+            
+            return RedirectToPage();
         }
         
     }
