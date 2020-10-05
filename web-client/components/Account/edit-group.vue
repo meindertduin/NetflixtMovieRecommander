4506c5f30bbabb7af8147b5eaeafcdf1a4e4cb14
Meindert <meindertduin@hotmail.com>
2020-10-05 16:43:58 +0200

    styling edit page

--- web-client/components/Account/edit-group.vue
+++ web-client/components/Account/edit-group.vue
@@ -4,52 +4,57 @@
       Edit Group
     </v-card-title>
     <v-card-text>
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-text-field label="Group Title" v-model="editedTitle"></v-text-field>
-          <v-text-field label="Description" v-model="editedDescription"></v-text-field>
-          <v-row>
-            <v-chip-group column v-for="(x, index) in editedAddedNames" :key="index">
-              <v-chip close @click:close="removeAddedName(x)">{{x}}</v-chip>
-            </v-chip-group>
-          </v-row>
-          <v-row justify="center" align="center">
-            <v-text-field label="Add Name" v-model="AddNameFormValue"></v-text-field>
-            <v-btn text color="green" @click="AddName">Add</v-btn>
-          </v-row>
-          <v-row justify="center">
-            <v-col cols="12">
-              <v-btn @click="save" color="green">Save Changes</v-btn>
-              <v-btn @click="initFields" >Cancel</v-btn>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-row justify="center" align="center">
-            <InviteAsFollower :group-id="currentWatchGroup.id"/>
-          </v-row>
-          <h3 class="mb-3">Add Watchlist: </h3>
-          <v-row justify="center">
-            <AddWatchlist  :group-id="currentWatchGroup.id"/>
-          </v-row>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-row>
-            <v-card>
-              <v-card-title>Delete Group</v-card-title>
-              <v-card-text>
-                <div>Type <span class="red--text">Delete</span> and click the button to delete the watchgroup</div>
-                <v-text-field v-model="deleteGuardText"></v-text-field>
-              </v-card-text>
-              <v-card-actions>
-                <v-row justify="center">
-                  <v-btn @click="deleteWatchGroup">Delete</v-btn>
-                </v-row>
-              </v-card-actions>
-            </v-card>
-          </v-row>
-        </v-col>
-      </v-row>
+      <div class="edit-watchgroup-container">
+        <v-row>
+          <v-col>
+            <v-text-field label="Group Title" v-model="editedTitle"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-textarea label="Description" v-model="editedDescription"></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row class="watchgroup-names-row">
+          <v-chip-group column v-for="(x, index) in editedAddedNames" :key="index">
+            <v-chip close @click:close="removeAddedName(x)">{{x}}</v-chip>
+          </v-chip-group>
+        </v-row>
+        <v-row>
+          <v-text-field label="Add Name" v-model="AddNameFormValue"></v-text-field>
+          <v-btn text color="green" @click="AddName">Add</v-btn>
+        </v-row>
+        <v-row class="my-4" justify="center">
+          <v-btn class="mx-2" @click="save" color="green">Save Changes</v-btn>
+          <v-btn class="mx-2" @click="initFields" >Cancel</v-btn>
+        </v-row>
+        <hr class="m-4">
+
+        <v-row>
+          <div class="display-1 font-weight-bold white--text mt-4">Add Friend</div>
+        </v-row>
+        <InviteAsFollower :group-id="currentWatchGroup.id"/>
+        <hr>
+
+        <v-row>
+          <div class="display-1 font-weight-bold white--text mt-4">Add Watchlist</div>
+        </v-row>
+        <AddWatchlist class="w-100" :group-id="currentWatchGroup.id"/>
+
+        <hr class="m-4">
+        <v-row>
+          <div class="display-1 white--text font-weight-bold my-4">Delete Group</div>
+        </v-row>
+        <v-row>
+          <div class="p-4">
+            <div class="h1 white--text">Delete Group</div>
+            <div>Type <span class="red--text">Delete</span> and click the button to delete the watchgroup</div>
+            <v-text-field v-model="deleteGuardText"></v-text-field>
+            <v-btn @click="deleteWatchGroup">Delete</v-btn>
+          </div>
+        </v-row>
+      </div>
+
     </v-card-text>
     <v-card-actions>
       <v-row justify="center">
@@ -137,5 +142,31 @@
 </script>
 
 <style scoped>
+  .edit-watchgroup-container{
+    max-height: 400px;
+    max-width: 800px;
+    padding: 40px;
+    overflow: auto;
+    overflow-x: hidden;
+  }
 
+  .watchgroup-names-row{
+    max-width: 800px;
+  }
+
+  .edit-watchgroup-container::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  .edit-watchgroup-container::-webkit-scrollbar-track {
+    background-color: black;
+    border-radius: 10px;
+  }
+
+  .edit-watchgroup-container::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    height: 2px;
+    background-color:#171a1d;
+    -webkit-box-shadow: inset 0 0 6px rgba(90,90,90,0.7);
+  }
 </style>
